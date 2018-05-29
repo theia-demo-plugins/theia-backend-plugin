e90463fcdb55a45687830f8b7e63f7f965ab0694
Oleksandr Andriienko <oandriie@redhat.com>
2018-05-29 22:54:06 +0300

    Improve terminal api examples.
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const disposables: theia.Disposable[] = [];
 
 export namespace Commands {
     export const SimpleCommand: theia.Command = {
-        id: 'simple-plugin-command',
+        id: 'simple-backend-plugin-command',
         label: 'Command from simple server plugin'
     };
 
@@ -26,17 +26,17 @@ export namespace Commands {
 
     export const InformationModalMessageTest = {
         id: 'backend-plugin-information-modal-message-command',
-        label: "Test Information Modal Message Server Plugin Item"   
+        label: "Test Information Modal Message Server Plugin Item"
     };
 
     export const WarningMessageTest = {
         id: 'backend-plugin-warning-message-command',
-        label: "Test Warning Message Server Plugin Item"        
+        label: "Test Warning Message Server Plugin Item"
     };
 
     export const WarningModalMessageTest = {
         id: 'backend-plugin-warning-modal-message-command',
-        label: "Test Warning Modal Message Server Plugin Item"     
+        label: "Test Warning Modal Message Server Plugin Item"
     };
 
     export const ErrorMessageTest = {
@@ -46,33 +46,43 @@ export namespace Commands {
 
     export const ErrorModalMessageTest = {
         id: 'backend-plugin-error-modal-message-command',
-        label: "Test Error Modal Message Server Plugin Item"       
+        label: "Test Error Modal Message Server Plugin Item"
     };
 
-    export const CreateTerminalWithParams: theia.Command = {
-        id: "backend-plugin-test-terminal-created-with-help-parameters-command",
-        label: "Test terminal created with help parameteres"
+    export const CreateTerminalWithHelpArgs: theia.Command = {
+        id: "backend-plugin-terminal-created-with-help-args",
+        label: "Create terminal with help arguments Server plugin"
     }
 
     export const CreateTerminalWithOptions: theia.Command = {
-        id: "backend-plugin-test-terminal-created-with-help-options-command",
-        label: "Test terminal created with help options"
+        id: "backend-plugin-terminal-created-with-help-options",
+        label: "Create terminal with help options Server plugin"
     }
 
     export const SendTextToTheTerminal: theia.Command = {
-        id: "backend-plugin-test-send-text-to-the-command",
-        label: "Test send text to the terminal"
+        id: "backend-plugin-send-text-to-the-terminal",
+        label: "Send text to the terminal Server plugin"
     }
 
-    export const HideTerminal: theia.Command = {
-        id: "backend-plugin-test-hide-terminal",
-        label: "Test hide terminal after 5 sec"
+    export const HidePanelWithTerminal: theia.Command = {
+        id: "backend-plugin-hide-terminal-panel",
+        label: "Hide terminal panel after 3 sec Server plugin"
     }
 
-    export const SibscribeOnDidCloseTeminalEvent: theia.Command = {
-        id: "backend-plugin-test-subscibe-on-did-close-terminal-event",
-        label: "Test subscribe onDidCloseTerminal event"
-    } 
+    export const ShowTerminalWithDelay: theia.Command = {
+        id: "backend-plugin-show-terminal-with-delay",
+        label: "Show terminal after 3 sec Server plugin"
+    }
+
+    export const DisposeTerminal: theia.Command = {
+        id: "backend-plugin-dispose-terminal",
+        label: "Dispose terminal after 3 sec Server plugin"
+    }
+
+    export const SibscribeToOnDidCloseTeminalEvent: theia.Command = {
+        id: "backend-plugin-subscibe-on-did-close-terminal-event",
+        label: "Subscribe to onDidCloseTerminal event Server plugin"
+    }
 }
 
 export function start() {
@@ -116,50 +126,72 @@ export function start() {
     }));
 
     /*----------------- Terminal api ------------------*/
-    disposables.push(theia.commands.registerCommand(Commands.CreateTerminalWithParams, () => {
-        const terminal = theia.window.createTerminal("Bash Terminal", "sh", ["-l"]);
-        terminal.processId.then(id => {
-                    theia.window.onDidCloseTerminal(async (term) => {
-                        const curId = await term.processId;
-                        if (curId === id) {
-                            console.log("Terminal closed ", id);
-                        }
-                    }, id);
-                });
+    disposables.push(theia.commands.registerCommand(Commands.CreateTerminalWithHelpArgs, () => {
+        const terminal = theia.window.createTerminal("Sh Terminal", "sh", ["-l"]);
         terminal.show();
     }));
 
     disposables.push(theia.commands.registerCommand(Commands.CreateTerminalWithOptions, () => {
-        const terminal = createTerminalWithOptions();
+        const termOptions: theia.TerminalOptions = {
+            name: "Test terminal",
+            shellPath: "/bin/bash",
+            shellArgs: ["-l"],
+            env: {"HELLO": "Hello Theia."},
+            // cwd: "/home/user/projects/che" // any existed absolute path or url to the folder
+        }
+        const terminal = theia.window.createTerminal(termOptions);
         terminal.show();
-        }));
-    
+    }));
+
     disposables.push(theia.commands.registerCommand(Commands.SendTextToTheTerminal, () => {
         const terminal = createTerminalWithOptions();
         terminal.show();
-        terminal.sendText("Theai plugin terminal.");
+        terminal.sendText("clear && echo Theia plugin terminal.\n");
     }));
 
-    disposables.push(theia.commands.registerCommand(Commands.HideTerminal, () => {
+    disposables.push(theia.commands.registerCommand(Commands.HidePanelWithTerminal, () => {
         const terminal = createTerminalWithOptions();
         terminal.show();
         setTimeout(function() {
             terminal.hide();
-        }, 5000);
+        }, 3000);
     }));
 
-    disposables.push(theia.commands.registerCommand(Commands.SibscribeOnDidCloseTeminalEvent, () => {
+    disposables.push(theia.commands.registerCommand(Commands.ShowTerminalWithDelay, () => {
         const terminal = createTerminalWithOptions();
-        terminal.processId.then(id => {
-                    theia.window.onDidCloseTerminal(async (term) => {
-                        const curId = await term.processId;
-                        if (curId === id) {
-                            console.log("Terminal closed ", id);
-                        }
-                    }, id);
-                });
-        terminal.show();
+        setTimeout(function() {
+            terminal.show();
+        }, 3000);
     }));
+
+    disposables.push(theia.commands.registerCommand(Commands.DisposeTerminal, () => {
+        const terminal = createTerminalWithOptions();
+        terminal.show();
+        setTimeout(function() {
+            terminal.dispose();
+        }, 3000);
+    }));
+
+    disposables.push(theia.commands.registerCommand(Commands.SibscribeToOnDidCloseTeminalEvent, () => {
+        const terminal = createTerminalWithOptions();
+        terminal.show();
+        terminal.processId.then(id => {
+            theia.window.onDidCloseTerminal(async (term) => {
+                const curId = await term.processId;
+                if (curId === id) {
+                    console.log("Terminal closed, id: ", id);
+                }
+            }, id);
+        });
+    }));
+}
+
+function createTerminalWithOptions(): theia.Terminal {
+    const termOptions: theia.TerminalOptions = {
+        name: "Test terminal",
+        shellPath: "/bin/bash"
+    }
+    return theia.window.createTerminal(termOptions);
 }
 
 export function stop() {
@@ -168,16 +200,3 @@ export function stop() {
         disposable.dispose();
     }
 }
-
-function createTerminalWithOptions(): theia.Terminal {
-    const termOptions: theia.TerminalOptions = {
-        name: "Test terminal",
-        shellPath: "sh",
-        shellArgs: ["-l"],
-        env: {"TERM":"xterm-256color"},
-        // cwd: "/path/to/workspace" todo we need get workspace path from api
-    }
-    return theia.window.createTerminal(termOptions);
-}
-
-// todo Seems quick pick samples missed up.
